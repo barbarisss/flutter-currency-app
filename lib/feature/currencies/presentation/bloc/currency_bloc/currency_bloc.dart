6027a072b0bdeb86b7e833f8d5eb3cf25b9f58f0
Vlad Edelev <vlladedellev@gmail.com>
2023-08-17 16:59:00 +0300

    feature: added use cases to the currency bloc

--- lib/feature/currencies/presentation/bloc/currency_bloc/currency_bloc.dart
+++ lib/feature/currencies/presentation/bloc/currency_bloc/currency_bloc.dart
@@ -1,3 +1,5 @@
+import 'package:currency_app/feature/currencies/domain/use_case/get_all_currencies_historical_use_case.dart';
+import 'package:currency_app/feature/currencies/domain/use_case/get_all_currencies_use_case.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:currency_app/feature/currencies/domain/entity/currency/currency_entity.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
@@ -7,9 +9,15 @@ part 'currency_state.dart';
 part 'currency_bloc.freezed.dart';
 
 class CurrencyBloc extends Bloc<CurrencyEvent, CurrencyState> {
-  CurrencyBloc() : super(const CurrencyState.initial()) {
+  CurrencyBloc({
+    required this.getAllCurrenciesUseCase,
+    required this.getAllCurrenciesHistoricalUseCase,
+  }) : super(const CurrencyState.initial()) {
     on<CurrencyEvent>((event, emit) {
       // TODO: implement event handler
     });
   }
+
+  final GetAllCurrenciesUseCase getAllCurrenciesUseCase;
+  final GetAllCurrenciesHistoricalUseCase getAllCurrenciesHistoricalUseCase;
 }
