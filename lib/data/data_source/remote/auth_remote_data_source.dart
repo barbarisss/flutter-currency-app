02a9416cd1b41140ed08320ebcce8cf5aba68c53
Vlad Edelev <vlladedellev@gmail.com>
2023-08-30 16:49:05 +0300

    feature: added auth remore data source

---
+++ lib/data/data_source/remote/auth_remote_data_source.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthDataSourceImpl {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String> signIn(String email, String password) async {
+    User? firebaseUser = (await _firebaseAuth.signInWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (firebaseUser == null) {
+      print('firebaseUser is null');
+    }
+
+    return firebaseUser!.uid;
+  }
+
+  Future<String> signUp(String email, String password) async {
+    User? firebaseUser = (await _firebaseAuth.createUserWithEmailAndPassword(
+            email: email, password: password))
+        .user;
+
+    if (firebaseUser == null) {
+      print('firebaseUser is null');
+    }
+
+    return firebaseUser!.uid;
+  }
+
+  Future<String?> getProfileUserEmail() async {
+    User? firebaseUser = _firebaseAuth.currentUser;
+    if (firebaseUser != null) {
+      return firebaseUser.email;
+    }
+    return null;
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
