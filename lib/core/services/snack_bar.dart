6bd02e2f282e9674b6775a1f7b018972835b0cd3
Vlad Edelev <vlladedellev@gmail.com>
2023-08-31 22:28:04 +0300

    refactor(snack_bar): minor changes

--- lib/core/services/snack_bar.dart
+++ lib/core/services/snack_bar.dart
@@ -3,7 +3,7 @@ import 'package:currency_app/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SnackBarService {
+abstract class SnackBarService {
   static const red = Colors.red;
   static const white = AppColors.white;
   static const black = AppColors.black;
@@ -14,7 +14,6 @@ class SnackBarService {
 
     final snackBar = SnackBar(
       content: Row(
-        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Icon(
             Icons.report_gmailerrorred_rounded,
@@ -32,17 +31,6 @@ class SnackBarService {
         ],
       ),
       backgroundColor: error ? red : white,
-      // behavior: SnackBarBehavior.floating,
-      // shape: OutlineInputBorder(
-      //   borderSide: BorderSide(
-      //     color: error ? errorColor : okColor,
-      //     width: 2.r,
-      //   ),
-      //   borderRadius: BorderRadius.circular(8),
-      // ),
-      // elevation: 100,
-      // padding: EdgeInsets.all(0),
-      // margin: EdgeInsets.all(0),
     );
 
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
